1ff9842ff5c3edc96abb8f4c95011e98c4157ab9
Damis Garcia <damisgarcia@gmail.com>
2024-07-15 19:09:54 -0300

    configure build lib with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vInfiniteScroll',
+      fileName: 'v-infinite-scroll'
+    },
+    rollupOptions: {
+      external: ['vue', 'peerDepsExternal'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
